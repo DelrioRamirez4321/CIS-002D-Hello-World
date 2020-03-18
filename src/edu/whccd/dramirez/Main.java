3a50d8e6494d07842eb4490809e15dd27015b794
DelrioRamirez4321 <delrioramirez7813@gmail.com>
2020-03-18 01:03:27 -0700

    first git push from intellj

---
+++ src/edu/whccd/dramirez/Main.java
@@ -0,0 +1,11 @@
+package edu.whccd.dramirez;
+/*
+    the purpose of this application is to display Hello World!
+    author:     Delrio Ramirez
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello World");
+    }
+}
